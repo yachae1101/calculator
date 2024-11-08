9f696fdd461a4fc6e60b4a9f7cfd7530a927f444
yachae1101 <yachae1101@naver.com>
2024-11-08 14:45:42 +0900

    code changed

--- src/main/java/com/bh/calculator/CalculatorController.java
+++ src/main/java/com/bh/calculator/CalculatorController.java
@@ -12,7 +12,6 @@ public class CalculatorController {
 
     @RequestMapping("/")
     String sum(@RequestParam("a") Integer a, @RequestParam("b") Integer b) {
-        //
         return String.valueOf(calculator.sum(a, b));
     }
 }
